51f81da24fe670f2116c98fcfbe42cba69001e0a
Tejal Dhargalkar <52788803+tejaldhargalkar27@users.noreply.github.com>
2025-03-04 18:21:49 +0530

    Update TransactionController.java

--- src/main/ java/com/example/moneylaundering/controller/TransactionController.java
+++ src/main/ java/com/example/moneylaundering/controller/TransactionController.java
@@ -5,37 +5,39 @@ import com.example.moneylaundering.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/transactions")
+@RequestMapping("/api/transactions")
 public class TransactionController {
 
     @Autowired
     private TransactionService transactionService;
 
     @GetMapping
-    public List<Transaction> getAllTransactions() {
-        return transactionService.getAllTransactions();
-    }
-
-    @PostMapping
-    public Transaction createTransaction(@RequestBody Transaction transaction) {
-        return transactionService.createTransaction(transaction);
+    public List<Transaction> getAllTransactions() throws IOException {
+        return transactionService.findAll();
     }
 
     @GetMapping("/{id}")
-    public Transaction getTransactionById(@PathVariable String id) {
-        return transactionService.getTransactionById(id);
+    public Transaction getTransactionById(@PathVariable String id) throws IOException {
+        return transactionService.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public void createTransaction(@RequestBody Transaction transaction) throws IOException {
+        transactionService.save(transaction);
     }
 
     @PutMapping("/{id}")
-    public Transaction updateTransaction(@PathVariable String id, @RequestBody Transaction transaction) {
-        return transactionService.updateTransaction(id, transaction);
+    public void updateTransaction(@PathVariable String id, @RequestBody Transaction transaction) throws IOException {
+        transaction.setTransactionId(id);
+        transactionService.update(transaction);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteTransaction(@PathVariable String id) {
-        transactionService.deleteTransaction(id);
+    public void deleteTransaction(@PathVariable String id) throws IOException {
+        transactionService.delete(id);
     }
 }
