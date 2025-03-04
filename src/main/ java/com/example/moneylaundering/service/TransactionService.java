988e0a2ab4baef64e18b96875cde9c46baeb14c7
Tejal Dhargalkar <52788803+tejaldhargalkar27@users.noreply.github.com>
2025-03-04 18:26:34 +0530

    Update TransactionService.java

--- src/main/ java/com/example/moneylaundering/service/TransactionService.java
+++ src/main/ java/com/example/moneylaundering/service/TransactionService.java
@@ -1,45 +1,37 @@
 package com.example.moneylaundering.service;
 
 import com.example.moneylaundering.model.Transaction;
+import com.example.moneylaundering.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TransactionService {
-    private List<Transaction> transactions = new ArrayList<>();
 
-    public List<Transaction> getAllTransactions() {
-        return transactions;
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    public List<Transaction> findAll() throws IOException {
+        return transactionRepository.findAll();
     }
 
-    public Transaction createTransaction(Transaction transaction) {
-        transactions.add(transaction);
-        return transaction;
+    public Optional<Transaction> findById(String transactionId) throws IOException {
+        return transactionRepository.findById(transactionId);
     }
 
-    public Transaction getTransactionById(String id) {
-        return transactions.stream()
-                .filter(transaction -> transaction.getTransactionId().equals(id))
-                .findFirst()
-                .orElse(null);
+    public void save(Transaction transaction) throws IOException {
+        transactionRepository.save(transaction);
     }
 
-    public Transaction updateTransaction(String id, Transaction updatedTransaction) {
-        Transaction transaction = getTransactionById(id);
-        if (transaction != null) {
-            transaction.setAccountId(updatedTransaction.getAccountId());
-            transaction.setTransactionType(updatedTransaction.getTransactionType());
-            transaction.setAmount(updatedTransaction.getAmount());
-            transaction.setTimestamp(updatedTransaction.getTimestamp());
-            transaction.setCounterparty(updatedTransaction.getCounterparty());
-            transaction.setLocation(updatedTransaction.getLocation());
-        }
-        return transaction;
+    public void update(Transaction transaction) throws IOException {
+        transactionRepository.update(transaction);
     }
 
-    public void deleteTransaction(String id) {
-        transactions.removeIf(transaction -> transaction.getTransactionId().equals(id));
+    public void delete(String transactionId) throws IOException {
+        transactionRepository.delete(transactionId);
     }
 }
