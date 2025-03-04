1de9c83bd805fcafb3e26c287b075d1174ebced3
Tejal Dhargalkar <52788803+tejaldhargalkar27@users.noreply.github.com>
2025-03-04 18:17:59 +0530

    Update AccountController.java

--- src/main/ java/com/example/moneylaundering/controller/AccountController.java
+++ src/main/ java/com/example/moneylaundering/controller/AccountController.java
@@ -5,37 +5,39 @@ import com.example.moneylaundering.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/accounts")
+@RequestMapping("/api/accounts")
 public class AccountController {
 
     @Autowired
     private AccountService accountService;
 
     @GetMapping
-    public List<Account> getAllAccounts() {
-        return accountService.getAllAccounts();
-    }
-
-    @PostMapping
-    public Account createAccount(@RequestBody Account account) {
-        return accountService.createAccount(account);
+    public List<Account> getAllAccounts() throws IOException {
+        return accountService.findAll();
     }
 
     @GetMapping("/{id}")
-    public Account getAccountById(@PathVariable String id) {
-        return accountService.getAccountById(id);
+    public Account getAccountById(@PathVariable String id) throws IOException {
+        return accountService.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public void createAccount(@RequestBody Account account) throws IOException {
+        accountService.save(account);
     }
 
     @PutMapping("/{id}")
-    public Account updateAccount(@PathVariable String id, @RequestBody Account account) {
-        return accountService.updateAccount(id, account);
+    public void updateAccount(@PathVariable String id, @RequestBody Account account) throws IOException {
+        account.setAccountId(id);
+        accountService.update(account);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteAccount(@PathVariable String id) {
-        accountService.deleteAccount(id);
+    public void deleteAccount(@PathVariable String id) throws IOException {
+        accountService.delete(id);
     }
 }
