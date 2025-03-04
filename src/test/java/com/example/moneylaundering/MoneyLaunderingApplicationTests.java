e669418069d1c0f700c7628e499c6e8fb23f0365
Tejal Dhargalkar <52788803+tejaldhargalkar27@users.noreply.github.com>
2025-03-04 15:37:14 +0530

    Create MoneyLaunderingApplicationTests.java

---
+++ src/test/java/com/example/moneylaundering/MoneyLaunderingApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.moneylaundering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyLaunderingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
