a5ea3032b417bd6b1f100f5e303e12151c69d92d
Tejal Dhargalkar <52788803+tejaldhargalkar27@users.noreply.github.com>
2025-03-04 15:10:17 +0530

    Update AMLResponse.java

--- src/main/ java/com/example/moneylaundering/model/AMLResponse.java
+++ src/main/ java/com/example/moneylaundering/model/AMLResponse.java
@@ -1,1 +1,33 @@
+package com.example.moneylaundering.model;
 
+public class AMLResponse {
+    private boolean isSuspicious;
+    private String riskScore;
+    private String message;
+    
+    // Getters and Setters
+
+    public boolean isSuspicious() {
+        return isSuspicious;
+    }
+
+    public void setSuspicious(boolean suspicious) {
+        isSuspicious = suspicious;
+    }
+
+    public String getRiskScore() {
+        return riskScore;
+    }
+
+    public void setRiskScore(String riskScore) {
+        this.riskScore = riskScore;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
